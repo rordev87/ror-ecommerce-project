ea04723369a307570169f3f9902969d2c7feb01b
rordev87 <rordev87@proton.me>
2018-11-10 00:33:08 +0000

    added search and filter for weapons

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,10 +1,24 @@
 class WeaponsController < ApplicationController
   def index
-    @weapon_collection = Weapon.order(:name).select(params[:category]).page(params[:page])
-    # @weapon_collection = w@eapon_collection.starts_with(params[:starts_with]) if params[:starts_with].present?
+    @categories = Category.all
+
+    if params[:search] && params[:filter]
+      @weapon_collection = Weapon.order(:name).page(params[:page]).where('name LIKE ?', "%#{params[:search]}%").where("category_id = ?", params[:filter][:category_id])
+    elsif params[:search]
+       @weapon_collection = Weapon.order(:name).page(params[:page]).where('name LIKE ?', "%#{params[:search]}%")
+    elsif params[:filter]
+      @weapon_collection = Weapon.order(:name).page(params[:page]).where("category_id = ?", params[:filter][:category_id])
+    else
+      @weapon_collection = Weapon.order(:name).page(params[:page])
+    end
   end
 
   def show
     @weapon = Weapon.find(params[:id])
   end
+
+  private
+    def weapon_params
+      params.require(:search, :filter).permit(:search, :filter)
+    end
 end
