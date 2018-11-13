7e1a2c359d8e5a1f3837da9993996981ceff2a35
rordev87 <rordev87@proton.me>
2018-11-13 18:52:10 +0000

    Add user registration/sign etc using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/create'
-  get 'users/new'
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :weapons
-  resources :users
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
+  get '/checkout' to: 'pages#checkout'
 
   root :to => "pages#home"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_184051) do
+ActiveRecord::Schema.define(version: 2018_11_13_152143) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
